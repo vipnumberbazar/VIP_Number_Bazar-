dd9954f01aa054a6985b03c823f7679bcc06a85d
vipnumberbazar <vipnumberbazar.73@gmail.com>
2026-07-07 12:47:48 +0530

    Ok1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 import { db } from "./firebase.js";
 import {
   collection,
@@ -10,62 +9,78 @@ let vipNumbers = [];
 const container = document.getElementById("numbers");
 
 async function loadNumbers() {
-  const querySnapshot = await getDocs(collection(db, "vipNumbers"));
+  try {
+    const querySnapshot = await getDocs(collection(db, "vipNumbers"));
 
-  vipNumbers = [];
+    vipNumbers = [];
 
-  querySnapshot.forEach((doc) => {
-    vipNumbers.push(doc.data());
-  });
+    querySnapshot.forEach((doc) => {
+      vipNumbers.push(doc.data());
+    });
 
-  showNumbers(vipNumbers);
-}
-function searchNumber(){
-
-const text=document
-.getElementById("searchBox")
-.value
-.toLowerCase();
-
-const result = vipNumbers.filter(item =>
-item.number.replace(/\s/g,"").includes(text.replace(/\s/g,""))
-);
-
-showNumbers(result);
-
-}
-function filterOperator(operator){
-
-if(operator==="All"){
-showNumbers(vipNumbers);
-return;
+    showNumbers(vipNumbers);
+  } catch (e) {
+    console.error(e);
+    container.innerHTML = "<h2>Data Load Error</h2>";
+  }
 }
 
-const result = vipNumbers.filter(item => item.operator===operator);
-
-showNumbers(result);
-
-}
 function showNumbers(list) {
   container.innerHTML = "";
+
+  if (list.length === 0) {
+    container.innerHTML = "<h2>No VIP Numbers Found</h2>";
+    return;
+  }
 
   list.forEach(item => {
     container.innerHTML += `
       <div class="card">
         <h2>${item.number}</h2>
+
         <p><b>Operator:</b> ${item.operator}</p>
+
         <p><b>Price:</b> ₹${item.price}</p>
+
         <p><b>Status:</b> ${item.status}</p>
 
         <a class="buy-btn"
         href="https://wa.me/918070424242?text=I want VIP Number ${item.number}">
-          Book Now
+        Book Now
         </a>
+
       </div>
     `;
   });
 }
 
-loadNumbers();
+function searchNumber() {
+
+  const text = document
+    .getElementById("searchBox")
+    .value
+    .toLowerCase();
+
+  const result = vipNumbers.filter(item =>
+    item.number.replace(/\s/g, "").includes(text.replace(/\s/g, ""))
+  );
+
+  showNumbers(result);
+}
+
+function filterOperator(operator) {
+
+  if (operator === "All") {
+    showNumbers(vipNumbers);
+    return;
+  }
+
+  const result = vipNumbers.filter(item => item.operator === operator);
+
+  showNumbers(result);
+}
+
 window.searchNumber = searchNumber;
 window.filterOperator = filterOperator;
+
+loadNumbers();
